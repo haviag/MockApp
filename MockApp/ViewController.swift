a66eff9c99c89775c864cef64dc5c0ea51289e4e
haviag <81833396+haviag@users.noreply.github.com>
2024-01-29 15:05:51 -0500

    Initial Commit

---
+++ MockApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MockApp
+//
+//  Created by Harold Villacob on 29/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
